5103bfa73f1ed33dccd91af61cbed7d4be25b12a
Yaroslav Golinskiy <kaiserdem@gmail.com>
2020-02-03 19:07:49 +0200

    added separator for userProfileView

---
+++ Find Love/View/ProfileCells/AboutSelfViewCell/AboutSelfViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AboutSelfViewCell.swift
+//  Find Love
+//
+//  Created by Kaiserdem on 03.02.2020.
+//  Copyright © 2020 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class AboutSelfViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Find Love/View/ProfileCells/StatusViewCell/StatusViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatusViewCell.swift
+//  Find Love
+//
+//  Created by Kaiserdem on 03.02.2020.
+//  Copyright © 2020 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
